5e7649b598d9e99de64a42a5b50adf1ce82bb4ba
EvgenPrushk <ogmprushak@mail.ru>
2021-06-23 19:53:12 +0300

    refactor: post

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,9 +1,9 @@
-import { ADD_POST, LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKED } from "../types";
-import { DB } from "../../db";
 import FileSystem from "expo-file-system";
+import { LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKED, ADD_POST } from "../types";
+import { DB } from "../../db";
 
 export const loadPosts = () => {
-  return async (dispatch) => {
+  return async dispatch => {
     // get post in database
     const posts = await DB.getPosts();
     // dispath data in App
@@ -14,28 +14,28 @@ export const loadPosts = () => {
   };
 };
 
-export const toogleBooked = post => async (dispatch) => {
-  await DB.updatePost(post)
+export const toggleBooked = (post) => async (dispatch) => {
+  await DB.updatePost(post);
 
-  dispatch ({
+  dispatch({
     type: TOGGLE_BOOKED,
     payload: post.id,
   });
 };
 
 export const removePost = (id) => async (dispatch) => {
-  await DB.removePost(id)
-  dispatch ({
+  await DB.removePost(id);
+  dispatch({
     type: REMOVE_POST,
     payload: id,
   });
 };
 
-export const addPost = (post) => async dispatch => {
+export const addPost = (post) => async (dispatch) => {
   // file name
-  const fileName = post.img.split("/").pop();
+  const fileName = post.img.split('/').pop();
   const newPath = FileSystem.documentDirectory + fileName;
-  
+
   try {
     await FileSystem.moveAsync({
       to: newPath,
@@ -46,8 +46,8 @@ export const addPost = (post) => async dispatch => {
   }
 
   const payload = { ...post, img: newPath };
-
   const id = await DB.createPost(payload);
+
   payload.id = id;
 
   dispatch({
