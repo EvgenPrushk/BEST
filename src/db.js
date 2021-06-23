92d060809fa1c78bebfe5d69c09833ae61f8d72e
EvgenPrushk <ogmprushak@mail.ru>
2021-06-23 21:52:55 +0300

    refacotor: db

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ export class DB {
     return new Promise((resolve, reject) => {
       db.transaction((tx) => {
         tx.executeSql(
-          "CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY NOT NULL, text TEXT, img TEXT, date TEXT, booked INT)",
+          'CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY NOT NULL, text TEXT NOT NULL, img TEXT, date TEXT, booked INT)',
           [],
           resolve,
           (_, error) => reject(error)
@@ -21,7 +21,7 @@ export class DB {
     return new Promise((resolve, reject) => {
       db.transaction((tx) => {
         tx.executeSql(
-          "SELECT * FROM posts",
+          'SELECT * FROM posts',
           [],
           (_, result) => resolve(result.rows._array),
           (_, error) => reject(error)
@@ -30,7 +30,7 @@ export class DB {
     });
   }
 
-  static createPost({ text, date, img }) {
+  static createPost({ text, date, booked, img }) {
     return new Promise((resolve, reject) => {
       db.transaction((tx) => {
         tx.executeSql(
