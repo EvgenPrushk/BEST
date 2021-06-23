feed2bc3b8d6b53ec0c947c8ef9dc77e58de14b3
EvgenPrushk <ogmprushak@mail.ru>
2021-06-23 20:32:57 +0300

    refactor: postScreen

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -33,8 +33,8 @@ export const PostScreen = ({ navigation }) => {
 
   const toggleHandler = useCallback(() => {
     console.log(postId);
-    dispatch(toogleBooked(postId));
-  }, [dispatch, postId]);
+    dispatch(toogleBooked(post));
+  }, [dispatch, post]);
 
   useEffect(() => {
     navigation.setParams({ toggleHandler });
